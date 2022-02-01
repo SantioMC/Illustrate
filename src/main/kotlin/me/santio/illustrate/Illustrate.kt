113d7c0a7c918853bbe86ccb58e6b2bcf36fff68
Santio <santiothebest123@gmail.com>
2022-02-01 14:42:42 -0500

    Initial commit

---
+++ src/main/kotlin/me/santio/illustrate/Illustrate.kt
@@ -0,0 +1,13 @@
+package me.santio.illustrate
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Illustrate : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
