ad6cad3158ea0b23bd1ebd4bfa2af937f3dfbd21
Santio <santiothebest123@gmail.com>
2022-02-01 14:44:38 -0500

    Initial Setup

--- src/main/kotlin/me/santio/illustrate/Illustrate.kt
+++ src/main/kotlin/me/santio/illustrate/Illustrate.kt
@@ -1,10 +1,17 @@
 package me.santio.illustrate
 
+import me.santio.utils.SantioUtils
 import org.bukkit.plugin.java.JavaPlugin
 
 class Illustrate : JavaPlugin() {
+
+    companion object {
+        lateinit var utils: SantioUtils
+        fun get(): Illustrate = getPlugin(Illustrate::class.java)
+    }
+
     override fun onEnable() {
-        // Plugin startup logic
+        utils = SantioUtils(this)
     }
 
     override fun onDisable() {
