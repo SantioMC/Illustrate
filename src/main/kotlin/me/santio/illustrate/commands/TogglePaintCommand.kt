d34586c9dca81a584d1f157c3c395b167ccb55ca
Santio <santiothebest123@gmail.com>
2022-02-04 17:31:29 -0500

    Bug fixes

--- src/main/kotlin/me/santio/illustrate/commands/TogglePaintCommand.kt
+++ src/main/kotlin/me/santio/illustrate/commands/TogglePaintCommand.kt
@@ -11,12 +11,12 @@ import org.bukkit.entity.Player
 
 object TogglePaintCommand: CommandExecutor, TabCompleter {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if (sender !is Player && args.isEmpty() || (args[0].lowercase() != "all" && Bukkit.getWorld(args[0]) == null)) {
+        if (sender !is Player && args.isEmpty() || (args.isNotEmpty() && args[0].lowercase() != "all" && Bukkit.getWorld(args[0]) == null)) {
             sender.sendMessage(ChatUtils.tacc("&c/togglepaint <world>"))
             return true
         }
 
-        if (args[0].lowercase() == "all" || (args.isEmpty() && sender is Player)) {
+        if ((args.isEmpty() && sender is Player) || args.isNotEmpty() && args[0].lowercase() == "all") {
             Illustrate.paintingDisabled = !Illustrate.paintingDisabled
             Bukkit.broadcastMessage(ChatUtils.tacc(
                 if (Illustrate.paintingDisabled) "\n&7Painting has been &4disabled &7by &c${sender.name}&7!\n"
@@ -28,8 +28,8 @@ object TogglePaintCommand: CommandExecutor, TabCompleter {
             else Illustrate.disabledWorlds.add(world)
             Bukkit.getOnlinePlayers().filter { p -> p.world.name.lowercase() == world }.forEach { p ->
                 p.sendMessage(ChatUtils.tacc(
-                    if (Illustrate.disabledWorlds.contains(world)) "\n&7Painting in this world has been &4disabled&7 by &c${sender.name}\n"
-                    else "\n&7Painting in this world has been &2enabled&7 by &a${sender.name}\n"
+                    if (Illustrate.disabledWorlds.contains(world)) "\n&7Painting in this world has been &4disabled&7 by &c${sender.name}\n&f"
+                    else "\n&7Painting in this world has been &2enabled&7 by &a${sender.name}\n&f"
                 ))
             }
         }
